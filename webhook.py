41981a36bbd1a6ab04538587b26a89c6579f56ce
Loic Chazeau <72005527+LoicChazeau@users.noreply.github.com>
2024-09-05 15:52:40 +0200

    Create webhook.py

---
+++ webhook.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/git-update", methods=["POST"])
+def git_update():
+    if request.method == "POST":
+        os.system("cd /home/ec2-user/Fc-Tryhard-bot && git pull")
+        return "Git Pull effectué", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
