38c09adefb42b7e6f87a931a874cdbe6379ec10e
Loic Chazeau <72005527+LoicChazeau@users.noreply.github.com>
2024-09-05 16:03:20 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import os
-#test
+
 app = Flask(__name__)
 
 @app.route("/git-update", methods=["POST"])
@@ -10,4 +10,4 @@ def git_update():
         return "Git Pull effectué", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
