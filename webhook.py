6fff21c4cf179e5eb8e5aded73e4efa15c167b18
Loic Chazeau <72005527+LoicChazeau@users.noreply.github.com>
2024-09-05 16:39:45 +0200

    auto launch and kill when git push

--- webhook.py
+++ webhook.py
@@ -1,13 +1,28 @@
 from flask import Flask, request
 import os
+import subprocess
 
 app = Flask(__name__)
 
-@app.route("/git-update", methods=["POST"])
+@app.route('/git-update', methods=['POST'])
 def git_update():
-    if request.method == "POST":
+    if request.method == 'POST':
+        # Aller dans le dossier du projet et faire un git pull
         os.system("cd /home/ec2-user/Fc-Tryhard-bot && git pull")
-        return "Git Pull effectué", 200
+
+        # Relancer le bot
+        restart_bot()
+        
+        return 'Update successful', 200
+    else:
+        return 'Invalid request', 400
+
+def restart_bot():
+    # Arrêter le bot s'il est en cours d'exécution
+    os.system("pkill -f main.py")  # Arrête le processus Python en fonction du nom du fichier
+    
+    # Lancer le bot dans un nouvel écran (screen session)
+    os.system("screen -dmS bot_screen python3 /home/ec2-user/Fc-Tryhard-bot/main.py")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
